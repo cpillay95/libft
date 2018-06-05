8c720c5f8be6a25adba060f910bdd12c61b79313
Cassandra PILLAY <cpillay@e4r14p6.wethinkcode.co.za>
2018-06-05 15:55:55 +0200

    end

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,12 +6,11 @@
 /*   By: cpillay <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 12:07:57 by cpillay           #+#    #+#             */
-/*   Updated: 2018/06/04 16:21:17 by cpillay          ###   ########.fr       */
+/*   Updated: 2018/06/05 15:52:19 by cpillay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 static int	count_split(const char *str, char c)
 {
 	int i;
@@ -19,7 +18,7 @@ static int	count_split(const char *str, char c)
 
 	i = 0;
 	ret = 0;
-
+	
 	while (str[i] != '\0')
 	{
 		while (str[i] == c)
@@ -36,7 +35,7 @@ static int	count_split(const char *str, char c)
 	return (ret);
 }
 
-static char	**string_split (char **ret, char *str, char c, int count)
+static char	**string_split (char **ret, const char *str, char c)
 {
 	int i;
 	int j;
@@ -44,38 +43,39 @@ static char	**string_split (char **ret, char *str, char c, int count)
 
 	i = 0;
 	j = 0;
-	count = count + 1;
 
-	while (i < count)
-	{
 		while (str[j] != '\0')
 		{
 			if (str[j] != c && str[j] != '\0')
 			{
-				k = 0;
+				k = j;
 				while (str[j] != c && str[j] != '\0')
-					k++;
+				{	
+					j++;
+				}
+				ret[i] = ft_strsub(str, k, (j - k ));
+				i++;
 			}
 			j++;
 		}
-		i++;
-	}
-	ret[i] = ft_strsub(str, i, i + k);
 	ret[i] = NULL;
 	return (ret);
-}
+} 
 
 char		**ft_strsplit(char const *s, char c)
 {
 	int count;
 	char **ret;
-	char *new;
 
-	new = (char*)s;
-	count = count_split(s, c);
-	ret = (char**)malloc(sizeof(char*) * count + 1);
-	ret = string_split(ret, new, c, count + 1);
-
-	return (ret);
+	if (s)
+	{
+		count = count_split(s, c);
+		ret = NULL;
+		if(!(ret = (char**)malloc((1 + count) * sizeof(char*))))
+			return (NULL);
+		else
+			string_split(ret, s, c);
+		return (ret);
+	}
+	return (NULL);
 }
-
